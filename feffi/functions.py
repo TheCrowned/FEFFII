99ba2c148f296e662469b0ebc34a50df6cdf9552
Stefano Ottolenghi <ottolenghi@protonmail.com>
2020-10-15 10:06:46 +0200

    Added pressure shift

--- feffi/functions.py
+++ feffi/functions.py
@@ -120,7 +120,7 @@ def define_variational_problems(f, mesh, **kwargs):
     p = f['p']; p_n = f['p_n']; q = f['q']; p_ = f['p_']
     T = f['T']; T_n = f['T_n']; T_v = f['T_v']
     S = f['S']; S_n = f['S_n']; S_v = f['S_v']
-    rho_0 = config_l['rho_0'];
+    rho_0 = config_l['rho_0']; g = config_l['g'];
 
     # Assemble tensor viscosity/diffusivity
     nu = feffi.parameters.assemble_viscosity_tensor(config_l['nu']);
@@ -149,11 +149,12 @@ def define_variational_problems(f, mesh, **kwargs):
     stiffness_mats = {}; load_vectors = {}
 
     # Define variational problem for approximated velocity
-    buoyancy = fenics.Expression((0, '-g*(1 -beta*(T_ - T_0) + gamma*(S_ - S_0))'), beta=config_l['beta'], gamma=config_l['gamma'], T_0=config_l['T_0'], S_0=config_l['S_0'], g=config_l['g'], T_=f['T_'], S_=f['S_'], degree=2)
+    buoyancy = fenics.Expression((0, '-g*(-beta*(T_ - T_0) + gamma*(S_ - S_0))'), beta=config_l['beta'], gamma=config_l['gamma'], T_0=config_l['T_0'], S_0=config_l['S_0'], g=config_l['g'], T_=f['T_'], S_=f['S_'], degree=2)
+    y = fenics.Expression("1-x[1]", degree=2)
     F1 = + dot((u - u_n)/dt, v)*dx \
          + dot(dot(u_n, nabla_grad(u_n)), v)*dx \
-         + inner(sigma(U, p_n/rho_0), epsilon(v))*dx \
-         + dot(p_n*n/rho_0, v)*ds - dot(elem_mult(nu, nabla_grad(U))*n, v)*ds \
+         + inner(sigma(U, (p_n - rho_0*g*y)/rho_0), epsilon(v))*dx \
+         + dot((p_n - rho_0*g*y)*n/rho_0, v)*ds - dot(elem_mult(nu, nabla_grad(U))*n, v)*ds \
          - dot(buoyancy, v)*dx
     stiffness_mats['a1'], load_vectors['L1'] = fenics.lhs(F1), fenics.rhs(F1)
 
