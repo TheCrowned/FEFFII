d8f07b3e463f991a23d5d2702e583b5371b9a675
Stefano Ottolenghi <ottolenghi@protonmail.com>
2020-12-10 11:12:32 +0100

    Making the transport instability in convection diffusion equation more general, and make it work in 2D also.

--- experiments/1D-transport-instability.py
+++ experiments/1D-transport-instability.py
@@ -1,40 +1,58 @@
 from fenics import *
+import numpy as np
 import matplotlib.pyplot as plt
 
 '''
 See https://quickmathintuitions.org/reproducing-transport-instability-convection-diffusion-equation/
-The instability shows up with epsilon = 0.01 and mesh h = 0.1 for example (set beta to 0).
-However, if we add an artificial viscosity coefficient, the issue can vanish. Try beta = 0.5 for ex.
-This ensures the term with epsilon always has a numerator not smaller than one order of magnitude than the denominator.
+The instability shows up with epsilon = 0.01 and mesh nx = 10 (so that h = 0.1) for example.
 '''
 
-mesh = UnitIntervalMesh(35)
-V = FunctionSpace(mesh, 'P', 1)
-bcu = [
-    DirichletBC(V, Constant(0), 'near(x[0], 0)'),
-    DirichletBC(V, Constant(0), 'near(x[0], 1)'),
-]
+dim = 2
+nx = 10
+epsilon  = 0.01
+delta = 0.05
+apply_GLS_stab = True
+
+if dim == 1:
+    mesh = UnitIntervalMesh(nx)
+elif dim == 2:
+    mesh = UnitSquareMesh(nx, nx)
+
+if apply_GLS_stab:
+    print('Running WITH stabilization')
+    output_name = 'vel_stab.png'
+else:
+    print('Running withOUT stabilization')
+    output_name = 'vel_nonstab.png'
+
+V = FunctionSpace(mesh, 'P', 1) #why having 2 here makes the problem vanish even with h = 0.1, epsilon = 0.01?? THINK about what is the role of the degree of functionspace wrt solution. How does it affect it? Visualize it.
+
+bcu = DirichletBC(V, Constant(0), 'on_boundary') #BC applied to whole boundary. This is what makes the solution illy constrained
+
 u = TrialFunction(V)
 v = TestFunction(V)
-u_  = Function(V)
+u_ = Function(V)
+b = Constant((1,)*dim) #the comma forces a list of size 1
 f  = Constant(1)
-epsilon  = Constant(0.01)
-beta = Constant(0.3)
-hmin = mesh.hmin()
 
-a = (epsilon+beta*hmin)*dot(u.dx(0), v.dx(0))*dx + u.dx(0)*v*dx
-L = v*dx
+a = epsilon*dot(grad(u), grad(v))*dx + dot(b, grad(u))*v*dx
+L = dot(f, v)*dx
+
+def L_operator(u):
+    '''GLS differential operator L'''
+
+    return -epsilon*div(grad(u)) + dot(b, grad(u))
+
+if apply_GLS_stab:
+    a += delta*dot(L_operator(u), L_operator(v))*dx
+    L += delta*dot(f, L_operator(v))*dx
 
 solve(a == L, u_, bcs=bcu)
 
 print("||u|| = %s, ||u||_8 = %s" % ( \
     round(norm(u_, 'L2'), 2), round(norm(u_.vector(), 'linf'), 3)
 ))
-#print(u_.vector().get_local())
 
-fig2 = plt.scatter(mesh.coordinates(), u_.compute_vertex_values())
-plt.savefig('velxy.png', dpi = 300)
-plt.close()
-
-#plot(mesh)
-#plt.show()
+fig = plot(u_)
+plt.colorbar(fig)
+plt.show()
