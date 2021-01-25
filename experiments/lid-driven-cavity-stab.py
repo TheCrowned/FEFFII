6a3dd5041353165e73e99c832c6f66a53f5e17ae
Stefano Ottolenghi <ottolenghi@protonmail.com>
2021-01-25 07:11:02 +0100

    csv output results from multiple gammap and nu

--- experiments/lid-driven-cavity-stab.py
+++ experiments/lid-driven-cavity-stab.py
@@ -3,203 +3,217 @@ O's feffi module, with the addition of CBS stabilization.
 
 """
 import fenics
+import csv
 import matplotlib.pyplot as plt
 from fenics import (dot, inner, elem_mult, grad,
                     nabla_grad, div, dx, ds, sym, Identity,
                     DirichletBC, Constant, assemble,
                     solve)
 
+output_dir = 'lid-driven-cavity-stab/'
+
 # Physical parameters used by lid-driven-cavity.yml
-final_time = 8
-steps_n = 800
+final_time = 10
+steps_n = 1000
 g = 0
-nu = 5e-3
+nu_list = [1, 1e-1, 5e-2, 1e-2, 8e-3, 5e-3, 2e-3, 1e-3, 5e-4]
+gammap_list = [0.25, 0.2, 0.15, 0.1, 0.075, 0.05]
 
-# Mesh
-nx = 100
-msh = fenics.UnitSquareMesh(nx, nx)
+csv_results = open(output_dir+'results.csv', 'w', newline='')
+csv_fieldnames = ['nu', 'gammap', 'error_u', 'error_p']
+csv_writer = csv.DictWriter(csv_results, fieldnames=csv_fieldnames)
+csv_writer.writeheader()
+#results_out = csv.writer(csv_results, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-# Function spaces P2/P1
-V = fenics.VectorFunctionSpace(msh, 'CG', 2)
-Q = fenics.FunctionSpace(msh, 'CG', 1)
+for nu in nu_list:
 
-# Boundary conditions (semi-manually implemented, so have to check Ste's
-# implementation). Only using classes...
-class Top(fenics.SubDomain):
-    def inside(self, x, on_boundary):
-        return fenics.near(x[1], 1) and on_boundary
+    for gammap in gammap_list:
+
+        # Mesh
+        nx = 100
+        msh = fenics.UnitSquareMesh(nx, nx)
+
+        # Function spaces P2/P1
+        V = fenics.VectorFunctionSpace(msh, 'CG', 2)
+        Q = fenics.FunctionSpace(msh, 'CG', 1)
+
+        # Boundary conditions (semi-manually implemented, so have to check Ste's
+        # implementation). Only using classes...
+        class Top(fenics.SubDomain):
+            def inside(self, x, on_boundary):
+                return fenics.near(x[1], 1) and on_boundary
 
 
-class No_Slip(fenics.SubDomain):
-    def inside(self, x, on_boundary):
-        return (
-            (fenics.near(x[1], 0) or fenics.near(x[0], 0) or fenics.near(x[0], 1))
-            and on_boundary)
+        class No_Slip(fenics.SubDomain):
+            def inside(self, x, on_boundary):
+                return (
+                    (fenics.near(x[1], 0) or fenics.near(x[0], 0) or fenics.near(x[0], 1))
+                    and on_boundary)
 
 
-top_right_corner = [0, 0]
-bcp = DirichletBC(
-    Q, Constant(0),
-    'near(x[0], {:f}) && near(x[1], {:f})'.format(*top_right_corner),
-    method='pointwise'
-)
-bc_noslip = DirichletBC(V, Constant((0, 0)), No_Slip())
-bc_top = DirichletBC(V, Constant((1, 0)), Top())
-bcs_v = [bc_noslip, bc_top]
+        top_right_corner = [0, 0]
+        bcp = DirichletBC(
+            Q, Constant(0),
+            'near(x[0], {:f}) && near(x[1], {:f})'.format(*top_right_corner),
+            method='pointwise'
+        )
+        bc_noslip = DirichletBC(V, Constant((0, 0)), No_Slip())
+        bc_top = DirichletBC(V, Constant((1, 0)), Top())
+        bcs_v = [bc_noslip, bc_top]
 
-# funcitons, trial functions and test functions
-u_n = fenics.Function(V)
-u = fenics.Function(V)
-v = fenics.TestFunction(V)
-p_n = fenics.Function(Q)
-p_n_1 = fenics.Function(Q)
-p_ = fenics.Function(Q)
-p = fenics.TrialFunction(Q)
-q = fenics.TestFunction(Q)
+        # funcitons, trial functions and test functions
+        u_n = fenics.Function(V)
+        u = fenics.Function(V)
+        v = fenics.TestFunction(V)
+        p_n = fenics.Function(Q)
+        p_n_1 = fenics.Function(Q)
+        p_ = fenics.Function(Q)
+        p = fenics.TrialFunction(Q)
+        q = fenics.TestFunction(Q)
 
-# Define expressions used in variational forms
-U = 0.5*(u_n + u)
-n = fenics.FacetNormal(msh)
-dt = final_time/steps_n
-gammap = 0.1 # gamma' #I tried 0.1, 0.15, 0.2, 0.5
-tau_s = dt*(1-gammap)
-tau_cg = dt/2
+        # Define expressions used in variational forms
+        U = 0.5*(u_n + u)
+        n = fenics.FacetNormal(msh)
+        dt = final_time/steps_n
+        #gammap = 0.1 # gamma' #I tried 0.1, 0.15, 0.2, 0.5
+        tau_s = dt*(1-gammap)
+        tau_cg = dt/2
 
-# VARIATIONAL FORMS for 2-eq splitting
-F1 = + dot((u - u_n)/dt, v)*dx \
-     + dot(dot(u, nabla_grad(u)), v)*dx \
-     + inner(2*nu*sym(nabla_grad(u)), sym(nabla_grad(v)))*dx \
-     - inner(((1+gammap)*p_n - gammap*p_n_1)*Identity(len(u)), sym(nabla_grad(v)))*dx \
-     + dot(((1+gammap)*p_n - gammap*p_n_1)*n, v)*ds \
-     - dot(nu*nabla_grad(u)*n, v)*ds \
-     - tau_cg*dot(dot(u_n, nabla_grad(v)), dot(u_n, nabla_grad(u_n)) - nu*u_n + grad(p_n))*dx
+        # VARIATIONAL FORMS for 2-eq splitting
+        F1 = + dot((u - u_n)/dt, v)*dx \
+             + dot(dot(u, nabla_grad(u)), v)*dx \
+             + inner(2*nu*sym(nabla_grad(u)), sym(nabla_grad(v)))*dx \
+             - inner(((1+gammap)*p_n - gammap*p_n_1)*Identity(len(u)), sym(nabla_grad(v)))*dx \
+             + dot(((1+gammap)*p_n - gammap*p_n_1)*n, v)*ds \
+             - dot(nu*nabla_grad(u)*n, v)*ds \
+             - tau_cg*dot(dot(u_n, nabla_grad(v)), dot(u_n, nabla_grad(u_n)) - nu*u_n + grad(p_n))*dx
 
-# Variational problem for pressure p with approximated velocity u
-F2 = + div(u)*q*dx \
-     + tau_s*dot(nabla_grad(p), nabla_grad(q))*dx
+        # Variational problem for pressure p with approximated velocity u
+        F2 = + div(u)*q*dx \
+             + tau_s*dot(nabla_grad(p), nabla_grad(q))*dx
 
-A2 = assemble(fenics.lhs(F2))
-bcp.apply(A2)
+        A2 = assemble(fenics.lhs(F2))
+        bcp.apply(A2)
 
-xdmffile = fenics.XDMFFile('lid-driven-cavity-stab/solutions.xdmf')
-xdmffile.parameters["flush_output"] = True
-xdmffile.parameters["functions_share_mesh"] = True
-u.rename("velocity", "Velocity in m/s")
-p_.rename("pressure", "Pressure in Pa")
-p_n_1.rename("old pressure", "Old pressure in Pa")
-t = 0
-no_time_steps = 0
-xdmffile.write(u, t)
-xdmffile.write(p_, t)
-xdmffile.write(p_n_1, t)
-
-solver_params = {'absolute_tolerance': 1e-4, 'maximum_iterations': 10}
-
-while no_time_steps <= steps_n:
-    # Applying IPC splitting scheme (IPCS)
-    # Step 1: Tentative velocity step
-    # A1 = self.A1
-    #b1 = assemble(L1)
-    #[bc.apply(b1) for bc in bcs_v]
-    #solve(A1, u_.vector(), b1)
-    solve(F1==0, u, bcs=bcs_v, solver_parameters={'newton_solver': solver_params})
-
-    # Step 2: Pressure correction step
-    # A2 = self.A2
-    b2 = assemble(fenics.rhs(F2))
-    bcp.apply(b2)
-    solve(A2, p_.vector(), b2)
-
-    # update solution
-    u_n.assign(u)
-    p_n_1.assign(p_n)
-    p_n.assign(p_)
-    t += dt
-    if not no_time_steps % 1:
+        xdmffile = fenics.XDMFFile(output_dir+'nu-{}/gammap-{}/solutions.xdmf'.format(nu, gammap))
+        xdmffile.parameters["flush_output"] = True
+        xdmffile.parameters["functions_share_mesh"] = True
+        u.rename("velocity", "Velocity in m/s")
+        p_.rename("pressure", "Pressure in Pa")
+        t = 0
+        no_time_steps = 0
         xdmffile.write(u, t)
-        xdmffile.write(p_n_1, t)
         xdmffile.write(p_, t)
-    no_time_steps += 1
-    print("Timestep: {}/{}".format(no_time_steps, steps_n))
 
-u_old = u
+        solver_params = {'relative_tolerance': 1e-4, 'maximum_iterations': 10}
 
-# naive implementation: Christian
-domain = fenics.UnitSquareMesh(100, 100)
-n = fenics.FacetNormal(domain)
-V_ = fenics.VectorElement("Lagrange", domain.ufl_cell(), 2)
-P_ = fenics.FiniteElement("Lagrange", domain.ufl_cell(), 1)
-W = fenics.FunctionSpace(domain, V_ * P_)
-Re = Constant(10**2)
+        while no_time_steps <= steps_n:
+            # Applying IPC splitting scheme (IPCS)
+            # Step 1: Tentative velocity step
+            # A1 = self.A1
+            #b1 = assemble(L1)
+            #[bc.apply(b1) for bc in bcs_v]
+            #solve(A1, u_.vector(), b1)
+            solve(F1==0, u, bcs=bcs_v, solver_parameters={'newton_solver': solver_params})
 
-w_trial = fenics.TrialFunction(W)
-w_test = fenics.TestFunction(W)
-(u, p) = fenics.split(w_trial)
-(v, q) = fenics.split(w_test)
-sol = fenics.Function(W)
-bcp_ = DirichletBC(
-    W.sub(1), Constant(0),
-    'near(x[0], {:f}) && near(x[1], {:f})'.format(*top_right_corner),
-    method='pointwise'
-)
-bc_noslip_ = DirichletBC(W.sub(0), Constant((0, 0)), No_Slip())
-bc_top_ = DirichletBC(W.sub(0), Constant((1, 0)), Top())
-bcs = [bcp, bc_top, bc_noslip]
-solver_params = {
-    'linear_solver': 'mumps',
-    'preconditioner': 'default',
-    'maximum_iterations': 10,
-    'relaxation_parameter': 1.0,
-    'relative_tolerance': 1e-7,
-}
+            # Step 2: Pressure correction step
+            # A2 = self.A2
+            b2 = assemble(fenics.rhs(F2))
+            bcp.apply(b2)
+            solve(A2, p_.vector(), b2)
 
-# Assemble tensor viscosity/diffusivity
-nu_tensor = fenics.as_tensor((
-    (nu, nu),
-    (nu, nu)))
+            # update solution
+            u_n.assign(u)
+            p_n_1.assign(p_n)
+            p_n.assign(p_)
+            t += dt
+            if not no_time_steps % 1:
+                xdmffile.write(u, t)
+                xdmffile.write(p_, t)
+            no_time_steps += 1
+            print("Timestep: {}/{}".format(no_time_steps, steps_n))
 
-# Define strain-rate tensor
-def epsilon(u):
-    return sym(nabla_grad(u))
+        u_old = u
+
+        # naive implementation: Christian
+        domain = fenics.UnitSquareMesh(100, 100)
+        n = fenics.FacetNormal(domain)
+        V_ = fenics.VectorElement("Lagrange", domain.ufl_cell(), 2)
+        P_ = fenics.FiniteElement("Lagrange", domain.ufl_cell(), 1)
+        W = fenics.FunctionSpace(domain, V_ * P_)
+        Re = Constant(10**2)
+
+        w_trial = fenics.TrialFunction(W)
+        w_test = fenics.TestFunction(W)
+        (u, p) = fenics.split(w_trial)
+        (v, q) = fenics.split(w_test)
+        sol = fenics.Function(W)
+        bcp_ = DirichletBC(
+            W.sub(1), Constant(0),
+            'near(x[0], {:f}) && near(x[1], {:f})'.format(*top_right_corner),
+            method='pointwise'
+        )
+        bc_noslip_ = DirichletBC(W.sub(0), Constant((0, 0)), No_Slip())
+        bc_top_ = DirichletBC(W.sub(0), Constant((1, 0)), Top())
+        bcs = [bcp, bc_top, bc_noslip]
+        solver_params = {
+            'linear_solver': 'mumps',
+            'preconditioner': 'default',
+            'maximum_iterations': 10,
+            'relaxation_parameter': 1.0,
+            'relative_tolerance': 1e-7,
+        }
+
+        # Assemble tensor viscosity/diffusivity
+        nu_tensor = fenics.as_tensor((
+            (nu, nu),
+            (nu, nu)))
+
+        # Define strain-rate tensor
+        def epsilon(u):
+            return sym(nabla_grad(u))
 
 
-# Define stress tensor
-def sigma(u, p, nu):
-    return 2*(nu*epsilon(u)) - p*Identity(len(u))
+        # Define stress tensor
+        def sigma(u, p, nu):
+            return 2*(nu*epsilon(u)) - p*Identity(len(u))
 
 
-def sigma_elem(u, p, nu_tensor):
-    return 2*elem_mult(nu_tensor, epsilon(u)) - p*Identity(len(u))
+        def sigma_elem(u, p, nu_tensor):
+            return 2*elem_mult(nu_tensor, epsilon(u)) - p*Identity(len(u))
 
-steady_formulation = (
-    + dot(dot(u, nabla_grad(u)), v)*dx
-    + nu*inner(nabla_grad(u), nabla_grad(v))*dx
-    - dot(p, fenics.div(v))*dx
-    - dot(fenics.div(u), q)*dx)
+        steady_formulation = (
+            + dot(dot(u, nabla_grad(u)), v)*dx
+            + nu*inner(nabla_grad(u), nabla_grad(v))*dx
+            - dot(p, fenics.div(v))*dx
+            - dot(fenics.div(u), q)*dx)
 
-steady_formulation_stress = (
-    + dot(dot(u, nabla_grad(u)), v)*dx
-    + inner(sigma_elem(u, p, nu_tensor), epsilon(v))*dx
-    - dot(fenics.div(u), q)*dx)
-F_vel = fenics.action(steady_formulation, sol)
-F_stress = fenics.action(steady_formulation_stress, sol)
-solve(F_stress == 0,
-      sol, bcs=[bcp_, bc_noslip_, bc_top_],
-      solver_parameters={'newton_solver': solver_params}
-)
+        steady_formulation_stress = (
+            + dot(dot(u, nabla_grad(u)), v)*dx
+            + inner(sigma_elem(u, p, nu_tensor), epsilon(v))*dx
+            - dot(fenics.div(u), q)*dx)
+        F_vel = fenics.action(steady_formulation, sol)
+        F_stress = fenics.action(steady_formulation_stress, sol)
+        solve(F_stress == 0,
+              sol, bcs=[bcp_, bc_noslip_, bc_top_],
+              solver_parameters={'newton_solver': solver_params}
+        )
 
-(u_sol, p_sol) = sol.split(True)
+        (u_sol, p_sol) = sol.split(True)
 
-xdmff = fenics.XDMFFile('lid-driven-cavity-stab/steady_solutions.xdmf')
-xdmff.parameters["flush_output"] = True
-xdmff.parameters["functions_share_mesh"] = True
-u_sol.rename("velocity", "Velocity in m/s")
-p_sol.rename("pressure", "Pressure in Pa")
-xdmff.write(u_sol, 0)
-xdmff.write(p_sol, 0)
+        xdmff = fenics.XDMFFile(output_dir+'nu-{}/gammap-{}/steady_solutions.xdmf'.format(nu, gammap))
+        xdmff.parameters["flush_output"] = True
+        xdmff.parameters["functions_share_mesh"] = True
+        u_sol.rename("velocity", "Velocity in m/s")
+        p_sol.rename("pressure", "Pressure in Pa")
+        xdmff.write(u_sol, 0)
+        xdmff.write(p_sol, 0)
 
-err = fenics.errornorm(u_old, u_sol, norm_type='L2')
-err_p = fenics.errornorm(p_, p_sol, norm_type='L2')
-print("nu: ", nu)
-print("||u_{time} - u_{steady}||_{L^2}: ", err)
-print("||p_{time} - p_{steady}||_{L^2}: ", err_p)
+        err = fenics.errornorm(u_old, u_sol, norm_type='L2')
+        err_p = fenics.errornorm(p_, p_sol, norm_type='L2')
+        print("nu: ", nu)
+        print("||u_{time} - u_{steady}||_{L^2}: ", err)
+        print("||p_{time} - p_{steady}||_{L^2}: ", err_p)
+
+        csv_writer.writerow({'nu': nu, 'gammap': gammap, 'error_u': err, 'error_p': err_p})
+        csv_results.flush()
