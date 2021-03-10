049a37176bb71deb545abb2fd46c576a8d8650ff
Stefano Ottolenghi <ottolenghi@protonmail.com>
2021-03-10 10:04:12 +0100

    Working?

--- examples/feffi_lid-driven-cavity.py
+++ examples/feffi_lid-driven-cavity.py
@@ -54,33 +54,6 @@ f = functions.define_functions(f_spaces)
 #functions.init_functions(f) # Init functions to closest steady state
 (stiffness_mats, load_vectors) = functions.define_variational_problems(f, mesh)
 
-## Apply CBS stabilization
-nu = parameters.assemble_viscosity_tensor(parameters.config['nu']);
-
-gamma = 1
-dt = 1/parameters.config['steps_n']
-tau = (2*gamma - 1)*dt/2
-F = fenics.Constant((0,0)) # g is null for lid driven cavity
-
-def R():
-    return dot(f['u_n'], nabla_grad(f['u_n'])) - nu*div(nabla_grad(f['u_n'])) + grad(f['p_n']) - F
-
-# Rebuild complete weak form and add stabilization term
-F1 = stiffness_mats['a1'] + load_vectors['L1']
-F1 += tau*dot(dot(f['u_n'], grad(f['v'])), R())*dx
-
-load_vectors['L1'] = fenics.rhs(F1)
-stiffness_mats['a1'] = fenics.lhs(F1)
-
-# Trying to see how much stab affects - no success
-#fenics.norm(delta*dot(grad(u_), R(u_))))
-#fenics.plot(delta*dot(grad(u_), R(u_))))
-#a = fenics.Expression('delta*dot(grad(u), R(u))', degree=2, delta=delta, u = f['u_'])
-#flog.info(fenics.norm(a))
-#plot(a)
-#plt.show()
-## End of CBS stabilization
-
 domain = boundaries.Domain(mesh, f_spaces)
 
 flog.info(
