2cbd1ecf677a5a62105b4d1919da0dc3387c45a4
Stefano Ottolenghi <ottolenghi@protonmail.com>
2021-05-26 12:09:06 +0200

    Experiment with ALE in LDC with crash

--- examples/feffi_lid-driven-cavity.py
+++ examples/feffi_lid-driven-cavity.py
@@ -6,115 +6,50 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from feffi import *
-import fenics
-from fenics import SubDomain, dot, nabla_grad, grad, div, dx
+import feffi
+from fenics import *
+from mshr import *
 import matplotlib.pyplot as plt
 
-parameters.define_parameters({
+feffi.parameters.define_parameters({
     'config_file' : 'feffi/config/lid-driven-cavity.yml',
 })
-parameters.parse_commandline_args()
+feffi.parameters.parse_commandline_args()
 
-# Uncomment this to use the COMSOL-like mesh, with rough interior and
-# progressively finer boundaries
-'''mesh = fenics.UnitSquareMesh(30,30)
+# Unstructured mesh
+#points = [Point(0,0), Point(1,0), Point(1,1), Point(0,1)] # square
+#points = [Point(0,0), Point(10,0), Point(10,1), Point(0,1)] # long domain
+#pentagon = Polygon(points)
+#mesh = generate_mesh(pentagon, 16, 'cgal')
 
-# Multiple passes of refinement from different distances from boundaries
-for thresh in [0.2, 0.1, 0.05, 0.025]:
-    class Bound_Top(SubDomain):
-        def inside(self, x, on_boundary):
-            return x[1] >= 1-thresh and x[0] <= 1-thresh
-    class Bound_Right(SubDomain):
-        def inside(self, x, on_boundary):
-            return x[0] >= 1-thresh and x[1] >= thresh
-    class Bound_Bottom(SubDomain):
-        def inside(self, x, on_boundary):
-            return x[1] <= thresh and x[0] >= thresh
-    class Bound_Left(SubDomain):
-        def inside(self, x, on_boundary):
-            return x[0] <= thresh and x[1] <= 1-thresh
+# Structured mesh
+mesh = UnitSquareMesh(8,8)
 
-    boundary_domain = fenics.MeshFunction("bool", mesh, mesh.topology().dim() - 1)
-    boundary_domain.set_all(False)
+feffi.plot.plot_single(mesh, display=True, title="Starting mesh")
 
-    boundariess = [Bound_Top, Bound_Right, Bound_Bottom, Bound_Left]
-    for boundary in boundariess:
-        obj = boundary()
-        obj.mark(boundary_domain, True)
+f_spaces = feffi.functions.define_function_spaces(mesh)
+f = feffi.functions.define_functions(f_spaces)
+domain = feffi.boundaries.Domain(mesh, f_spaces)
+simul = feffi.simulation.Simulation(f, domain.BCs)
 
-    mesh = fenics.refine(mesh, boundary_domain)'''
+print("Mesh hmin is {}".format(mesh.hmin()))
 
-mesh = mesh.create_mesh()
+# Repeatedly run 1 timestep and deform mesh
+for x in range(15):
+    simul.timestep()
 
-f_spaces = functions.define_function_spaces(mesh)
-f = functions.define_functions(f_spaces)
-#functions.init_functions(f) # Init functions to closest steady state
+    # 0.02 x-wise displacement
+    disp = Expression(("0.02*(left-x[0])", "0"),
+                      left=max(mesh.coordinates()[:,0]), degree=2)
 
-domain = boundaries.Domain(mesh, f_spaces)
+    # Move boundary and then mesh
+    #boundary = BoundaryMesh(mesh, 'exterior')
+    #ALE.move(boundary, disp)
+    #ALE.move(mesh, disp)
 
-flog.info(
-    '## Running lid driven benchmark with parameters \n{} ##'.format(
-        parameters.config))
+    # Move mesh according to given expression
+    ALE.move(mesh, disp)
 
-simul = simulation.Simulation(f, domain.BCs)
-simul.run()
-
-plot.plot_solutions(f)
-
-'''plot.plot_single(
-    mesh,
-    title='Mesh (nu = {})'.format(parameters.config['nu']),
-    display=False,
-    file_name='mesh-{}.png'.format(parameters.config['nu']),
-    )
-plot.plot_single(
-    f['u_'],
-    title='Velocity (nu = {})'.format(parameters.config['nu']),
-    display=False,
-    file_name='vel-{}.png'.format(parameters.config['nu']),
-    )
-plot.plot_single(
-    f['p_'],
-    title='Pressure (nu = {})'.format(parameters.config['nu']),
-    display=False,
-    file_name='pressure-{}.png'.format(parameters.config['nu']),
-    )'''
-
-# Export solutions for comparison
-#fenics.File('out/lid-driven-cavity_u_{}.xml'.format(parameters.config['nu'])) << f['u_']
-#fenics.File('out/lid-driven-cavity_p_{}.xml'.format(parameters.config['nu'])) << f['p_']
-
-# Compare with reference solutions
-'''ref_u = fenics.Function(f_spaces['V'].collapse())
-ref_p = fenics.Function(f_spaces['Q'].collapse())
-fenics.File(
-    'feffi/reference-solutions/lid-driven-cavity_u_{}.xml'.format(
-        parameters.config['nu'])) >> ref_u
-fenics.File(
-    'feffi/reference-solutions/lid-driven-cavity_p_{}.xml'.format(
-        parameters.config['nu'])) >> ref_p
-
-fig = plt.figure()
-ax = fig.add_subplot(2,3,1)
-pl = fenics.plot(f['u_'], title = 'Current u')
-plt.colorbar(pl)
-ax = fig.add_subplot(2,3,2)
-pl = fenics.plot(ref_u, title = 'Ref. u')
-plt.colorbar(pl)
-ax = fig.add_subplot(2,3,3)
-pl = fenics.plot(f['u_']-ref_u, title = 'Current - ref')
-plt.colorbar(pl)
-ax = fig.add_subplot(2,3,4)
-pl = fenics.plot(f['p_'], title = 'Current p')
-plt.colorbar(pl)
-ax = fig.add_subplot(2,3,5)
-pl = fenics.plot(ref_p, title = 'Ref. p')
-plt.colorbar(pl)
-ax = fig.add_subplot(2,3,6)
-pl = fenics.plot(f['p_']-ref_p, title = 'Current - ref')
-plt.colorbar(pl)
-
-plt.savefig('out/comp.png', dpi=800)
-plt.show()
-'''
+    #feffi.plot.plot_single(f['u_'], display=True)
+    feffi.plot.plot_single(mesh, display=True,
+                           title="Mesh with left x = {}".format(min(mesh.coordinates()[:,0])))
