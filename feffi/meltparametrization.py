38dca9e7278a48035c3980dcf48b13c21035c3b8
Stefano Ottolenghi <ottolenghi@protonmail.com>
2021-09-07 11:55:10 +0200

    * Correcting Ustar to have correct formulation in forcing terms. * (Some tests with dynamic coefficients gammaT, gammaS - commented out)

--- feffi/meltparametrization.py
+++ feffi/meltparametrization.py
@@ -1,6 +1,7 @@
 from fenics import (dx, Function, FunctionSpace, FiniteElement, lhs, rhs,
                     Constant, norm, MixedElement, TestFunctions, solve,
-                    TrialFunctions)
+                    TrialFunctions, ln)
+from . import parameters
 
 # introduce all constants
 # from Asai Davies 2016 ISOMIP Paper
@@ -12,9 +13,9 @@ L = Constant(3.34*10**5)
 lam1 = Constant(-0.0573)
 lam2 = Constant(0.0832)
 lam3 = Constant(-7.53*10**(-8))
-rhofw = Constant(1000)
-rhosw = Constant(1028)
-Ut = Constant(0.01) # Constant(0.01) ignore for now
+rhofw = Constant(1000)#/(3.6**2))
+rhosw = Constant(1028)#/(3.6**2))
+Ut = Constant(0.01)#*3.6) # Constant(0.01) ignore for now
 
 def solve_3eqs_system(uw, Tw, Sw, pzd):
     """
@@ -51,6 +52,8 @@ def solve_3eqs_system(uw, Tw, Sw, pzd):
     #Ustar = (Cd*norm(uw)**2+Ut**2)**(1/2) # Ustar^2 = Cd(Uw^2 + Ut^2)
     #Ustar = (Cd*uw**2+Ut**2)**(1/2) # Ustar^2 = Cd(Uw^2 + Ut^2) # ustar should be a function
     Ustar = (Cd*(uw.sub(0)**2+uw.sub(1)**2)+Ut**2)**(1/2)        # Ut is needed, maybe in case uw is 0 at some point?
+    #gamT = 1/(2.12*ln((Ustar*mesh.hmax())/max(parameters.config['nu']))+12.5*(max(parameters.config['nu'])/max(parameters.config['alpha']))-9) # from other paper
+
     # should Ustar be in some way only computed on the boundary??
     F = ( (+ rhofw*mw*L + rhosw*cw*Ustar*gammaT*(Tzd-Tw))*v_1*dx
            + (Tzd - lam1*Szd - lam2 - lam3*pzd)*v_2*dx
@@ -64,12 +67,14 @@ def solve_3eqs_system(uw, Tw, Sw, pzd):
 
 
 def build_heat_flux_forcing_term(u_, Tw, mw, Tzd):
-    Ustar = (Cd*norm(u_)**2+Ut**2)**(1/2) # Ustar^2 = Cd(Uw^2 + Ut^2)
+    #Ustar = (Cd*norm(u_)**2+Ut**2)**(1/2) # Ustar^2 = Cd(Uw^2 + Ut^2)
+    Ustar = (Cd*(u_.sub(0)**2+u_.sub(1)**2)+Ut**2)**(1/2)
     Fh = -cw*(rhosw*Ustar*gammaT+rhofw*mw)*(Tzd-Tw)
     return Fh
 
 
 def build_salinity_flux_forcing_term(u_, Sw, mw, Szd):
-    Ustar = (Cd*norm(u_)**2+Ut**2)**(1/2) # Ustar^2 = Cd(Uw^2 + Ut^2)
+    #Ustar = (Cd*norm(u_)**2+Ut**2)**(1/2) # Ustar^2 = Cd(Uw^2 + Ut^2)
+    Ustar = (Cd*(u_.sub(0)**2+u_.sub(1)**2)+Ut**2)**(1/2)
     Fs = -(rhosw*Ustar*gammaS+rhofw*mw)*(Szd-Sw)
     return Fs
